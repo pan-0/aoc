e0143090785f56a6d29f0e4dbb9d3a7f095dd736
pan <>
2024-12-04 08:34:03 +0200

    y24: add variant d04a-1.py
    More closely resembles the solution of part two.

---
+++ y24/d04a-1.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: 0BSD
+# Copyright (C) 2024 by pan <pan_@disroot.org>
+
+from __future__ import annotations
+
+import fileinput
+from pprint import pprint
+
+Mat = list[str]
+
+def parse(itr) -> Mat:
+    return [*map(str.strip, itr)]
+
+def xmas_n(s: str) -> int:
+    return sum(map(s.count, ("XMAS", "SAMX")))
+
+def dim(itr) -> int:
+    return sum(map(xmas_n, itr))
+
+def diag(M: Mat, i: int, j: int, sign: int) -> str:
+    return "".join(map(lambda offs: M[i + offs][j + sign * offs], range(4)))
+
+def f(M: Mat) -> int:
+    rows = dim(M)
+    cols = dim(map("".join, zip(*M)))
+    digs = 0
+    for i in range(0, len(M) - 3):
+        for j in range(len(M[i]) - 3):
+            diags = map(lambda offs, sign: diag(M, i, j + offs, sign),
+                        (0, 3),
+                        (1, -1))
+            digs += dim(diags)
+    return rows + cols + digs
+
+def main():
+    M = parse(fileinput.input())
+    r = f(M)
+    print(r)
+
+if __name__ == "__main__":
+    main()
+    raise SystemExit
+
