6d3d6d1cec8aab4292ed5db53c1c40b209e9c787
pan <>
2024-12-05 05:00:56 +0200

    y15/d04?.py: use islice

--- y15/d04a.py
+++ y15/d04a.py
@@ -2,7 +2,9 @@
 # Copyright (C) 2024 by pan <pan_@disroot.org>
 
 import hashlib
+import sys
 from functools import partial
+from itertools import islice
 
 def first(itr, key=lambda x: True):
     return next(filter(key, itr))
@@ -10,16 +12,16 @@ def first(itr, key=lambda x: True):
 def f(key: str, n: int):
     inp = (key + str(n)).encode("ascii")
     return (
-        int.from_bytes(hashlib.md5(inp).digest()[:3]) & 0xFFFFF0 == 0,
+        int.from_bytes(islice(hashlib.md5(inp).digest(), 3)) & 0xFFFFF0 == 0,
         n
     )
 
 def main():
     N = 1 << 31
-    key = input()
+    key = sys.argv[1]
     r = first(map(partial(f, key), range(N)), key=lambda p: p[0])
     print(r[1])
 
 if __name__ == "__main__":
     main()
-    raise SystemExit
+    sys.exit()
