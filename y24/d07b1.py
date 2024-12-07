2627652e338393aa8462c5451e7783b6b2984699
pan <>
2024-12-07 21:34:54 +0200

    y24/d07b1.py: double brainfart

--- y24/d07b1.py
+++ y24/d07b1.py
@@ -22,7 +22,7 @@ def is_sat(lhs: int, rhs: list[int], length: int) -> bool:
         return False
 
     q, r = divmod(lhs, last)
-    if (r == 0 and is_sat(q, rhs, end)) or is_sat(lhs - last, rhs, end)
+    if (r == 0 and is_sat(q, rhs, end)) or is_sat(lhs - last, rhs, end):
         return True
 
     q, r = divmod(lhs, 10 ** len(str(last)))
