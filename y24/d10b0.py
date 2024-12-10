09ba9185cc512732bdf8d8725e48b99a10ec1e42
pan <>
2024-12-10 09:00:57 +0200

    y24/d10b0.py: simplify

--- y24/d10b0.py
+++ y24/d10b0.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2024 by pan <pan_@disroot.org>
 
 import fileinput
-from itertools import chain
 from collections import defaultdict
 from string import digits
 
@@ -14,7 +13,7 @@ def parse(itr) -> Grid:
 def f(G: Grid) -> int:
     rows = len(G)
     cols = len(G[0])
-    trailheads = defaultdict(lambda: defaultdict(int))
+    trailheads = defaultdict(int)
     for i in range(rows):
         for j in range(cols):
             stack = [(0, i, j)]
@@ -22,15 +21,13 @@ def f(G: Grid) -> int:
                 c, x, y = stack.pop()
                 if 0 <= x < rows and 0 <= y < cols and G[x][y] == digits[c]:
                     if digits[c] == digits[-1]:
-                        trailheads[i, j][x, y] += 1
+                        trailheads[i, j, x, y] += 1
                     else:
                         stack.extend(((c + 1, x - 1, y),
                                       (c + 1, x + 1, y),
                                       (c + 1, x, y - 1),
                                       (c + 1, x, y + 1),))
-
-    return sum(chain.from_iterable(map(lambda tail: tail.values(),
-                                       trailheads.values())))
+    return sum(trailheads.values())
 
 def main():
     G = parse(fileinput.input())
