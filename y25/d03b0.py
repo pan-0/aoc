5dd8805fde194511a8105f5084b5144614bdd30c
pan <pan_@disroot.org>
2025-12-03 09:57:26 +0200

    y25: day 3 part 2

--- y25/d03b0.py
+++ y25/d03b0.py
@@ -10,13 +10,23 @@ from prelude import *
 def parse(itr: Iterator[str]) -> Iterator[list[str]]:
     return map(list, itr)
 
+def xmax(bank: list[str], start: int, end: int) -> int:
+    x = start
+    for i in range(x + 1, end):
+        if bank[i] > bank[x]:
+            x = i
+    return x
+
 def go(inp: Input) -> Iterator[int]:
     acc = 0
     for bank in parse(inp):
-        first = max(bank[:-1])
-        index = bank.index(first)
-        second = max(bank[index + 1:])
-        acc += int(f"{first}{second}")
+        bats = []
+        start = 0
+        for i in range(12 - 1, -1, -1):
+            x = xmax(bank, start, len(bank) - i)
+            start = x + 1
+            bats.append(bank[x])
+        acc += int("".join(bats))
     yield acc
 
 # {{{
